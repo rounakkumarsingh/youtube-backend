318ce3f8a37ed5722dfd305c476d1ecce79eb333
Rounak Kumar Singh <rounak.official.2@gmail.com>
2024-10-08 15:46:52 +0530

    setup done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
+import { asyncHandler } from "./utils/asyncHandler.js";
 
 dotenv.config({
     path: "../.env",
 });
-connectDB()
-    .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`App is listening on port ${process.env.PORT || 8000}`);
-        });
-    })
-    .catch((err) => {
-        console.log("MONGO CONNCTION ERROR: ", error);
+
+const startServer = asyncHandler(async () => {
+    await connectDB();
+    const server = app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is listening on port ${process.env.PORT || 8000}`);
     });
+});
+
+startServer().catch((error) => {
+    console.log("MONGO CONNCTION ERROR: ", error);
+});
+// connectDB()
+//     .then(() => {
+//         app.listen(process.env.PORT || 8000, () => {
+//             console.log(`App is listening on port ${process.env.PORT || 8000}`);
+//         });
+//     })
+//     .catch((err) => {
+//         console.log("MONGO CONNCTION ERROR: ", error);
+//     });
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,11 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn();
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message,
-        });
-    }
+const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+        try {
+            await requestHandler(req, res, next);
+        } catch (err) {
+            next(err);
+        }
+    };
 };
+
 export { asyncHandler };
