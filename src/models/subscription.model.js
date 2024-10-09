f5fb8261623224e0f42c06b8698b097fc340c8bd
Rounak Kumar Singh <rounak.official.2@gmail.com>
2024-10-09 10:17:40 +0530

    feat: add subscription model for user channel subscriptions

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+    {
+        subscriber: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        channel: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Subscription = model("Subscription", subscriptionSchema);
