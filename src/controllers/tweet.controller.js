621f9cb61e51e599cf6f1bf567ea30dc6b3c3de7
Rounak Kumar Singh <rounak.official.2@gmail.com>
2024-10-11 15:01:21 +0530

    feat: implement subscription management routes and controller; refactor tweet routes and models

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { User } from "../models/user.models.js";
-import { Tweet } from "../models/";
-import { ApiError } from "../utils/ApiError.js";
+import User  from "../models/user.model.js";
+import Tweet  from "../models/tweet.model.js";
+import ApiError from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { uplodOnCloudinary } from "../utils/cloudinary.js";
 
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
     createTweet,
     deleteTweet,
     updateTweet,
     getUserTweets,
 } from "../controllers/tweet.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
