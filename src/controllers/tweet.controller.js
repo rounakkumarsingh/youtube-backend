10016e8b21aad861e921f9be5e3c30e0c24164b1
Rounak Kumar Singh <rounak.official.2@gmail.com>
2024-10-11 11:39:21 +0530

    feat: add tweet routes and controller; implement tweet deletion functionality

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -64,9 +64,30 @@ const updateTweet = asyncHandler(async (req, res) => {
     tweet.content = newContent;
     const oldAttachments = tweet.attachments;
     tweet.attachments = newAttachments;
+    const updatedTweet = await tweet.save();
     await deleteFromCloudinary(oldAttachments)
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedTweet, "Tweet updated successfully"));
     
 });
-const deleteTweet = asyncHandler(async (req, res) => {});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    const tweetId = req.params;
+    const tweet = Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(404, "Tweet Not found");
+    }
+    if (tweet.owner !== req.user?._id) {
+        throw new ApiError(401, "You are not authorized to delete this tweet");
+    }
+
+    const attachments = tweet.attachments;
+    await tweet.delete();
+    await deleteFromCloudinary(attachments);
+    return res
+        .status(200)
+        .json(new ApiResponse(200, null, "Tweet deleted successfully"));
+});
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
